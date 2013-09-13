11eca082aa4b85913d529758dce10de6d6d5da47
Jan Halfar <jan@bestbytes.com>
2013-09-13 12:06:22 +0200

    removed test content service

--- htdocs/services/content.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('Content-Type: application/json');
-echo json_encode(
-	Foomo\Page\Content\Export::nodeToRepoNode(
-		\Bestbytes\Site\Module::getContentRootNode()
-	),
-	JSON_PRETTY_PRINT
-);
